602d2deaa6048a032938d269338310a5f966692f
Immanuel George <immanuel.george@vertical-knowledge.com>
2019-04-02 20:25:13 -0400

    setup files

--- setup.py
+++ setup.py
@@ -2,15 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='vklabs.tika-python',
-    version='0.0.1',
+    version='0.0.2',
     python_requires='>=3.6',
     packages=find_packages(),
     package_data={},
     install_requires=[
-        "setuptools"
-        "requests"
-        "python-coveralls"
-        "pytest-cov==2.5"
+        'setuptools',
+        'requests'
     ],
     entry_points='''
     ''',
