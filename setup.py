e7635cf52a5db2fd37162a3841426e78750e7064
Immanuel George <immanuel.george@vertical-knowledge.com>
2019-04-02 20:47:57 -0400

    bump

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='vklabs.tika',
-    version='0.0.3',
+    version='0.0.4',
     python_requires='>=3.6',
     packages=find_packages(),
     package_data={},
+    include_package_data=True,
     install_requires=[
         'setuptools',
         'requests'
